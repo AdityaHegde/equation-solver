3b597f912c602a3fce3a1f3fdf20a8486eed103e
Aditya Shridhar Hegde <adityahegderocks@gmail.com>
2014-11-26 18:23:13 +0530

    Added another layer of factorization
    a^2+2ab+b^2 => (a+b)^2

--- tests/main/eqn-factorize.js
+++ tests/main/eqn-factorize.js
@@ -10,44 +10,56 @@ TermBracket.TermMultiply = TermMultiply;
 
 module.exports = function() {
   describe("factorize", function() {
-    it("factorize a+a*b", function() {
-      var term = new TermBracket().fromString("a+a*b");
-      term = term.simplify();
-      term = term.factorize();
-      term.sortAndStringify();
-      term.fullStr.should.be.eql("(a+ab)");
-    });
+    var tests = [{
+    //*
+      input : "a+a*b",
+      output : "(a+ab)",
+    }, {
+      input : "a+a*b+a*c",
+      output : "(1+b+c)a",
+    }, {
+      input : "a+a*b+a*c+c*d+c*b",
+      output : "(cd+bc+(1+b+c)a)",
+    //*
+    }, {
+      input : "a^2+a*b+2*b^2",
+      output : "(a^2+ab+2b^2)",
+    }, {
+      input : "a^2+a*b+b^2",
+      output : "((-1)ab+(a+b)^2)",
+    }, {
+      input : "a^2+2*a*b+b^2",
+      output : "(a+b)^2",
+    }, {
+      input : "a^2+2*a*b+b^2+c^2+2*c*d+d^2",
+      output : "((a+b)^2+(c+d)^2)",
+    }, {
+      input : "a^2+2*a*b+b^2+c^2+2*c*d+d^2+e^2+e*f+e*g",
+      output : "((a+b)^2+(c+d)^2+(f+e+g)e)",
+    }, {
+      input : "a^2+2*a*b+b^2+e*c^2+2*e*c*d+e*d^2",
+      output : "((a+b)^2+(c+d)^2e)",
+    }, {
+      input : "a^4+4*a^3*b+6*a^2*b^2+4*b^3*a+b^4",
+      output : "(a+b)^4",
+    }, {
+    //*/
+      input : "a^4+4*a^3*b+6*a^2*b^2+4*b^3*a+b^4+a^6+b^2",
+      output : "((a+b)^4+(-2)a^3b+(a^3+b)^2)",
+    //*/
+    }];
 
-    it("factorize a+a*b+a*c", function() {
-      var term = new TermBracket().fromString("a+a*b+a*c");
-      term = term.simplify();
-      term = term.factorize();
-      term.sortAndStringify();
-      term.fullStr.should.be.eql("(1+b+c)a");
-    });
-
-    it("factorize a^2+a*b+2*b^2", function() {
-      var term = new TermBracket().fromString("a^2+a*b+2*b^2");
-      term = term.simplify();
-      term = term.factorize();
-      term.sortAndStringify();
-      term.fullStr.should.be.eql("(a^2+ab+2b^2)");
-    });
-
-    it("factorize a^2+a*b+b^2", function() {
-      var term = new TermBracket().fromString("a^2+a*b+b^2");
-      term = term.simplify();
-      term = term.factorize();
-      term.sortAndStringify();
-      term.fullStr.should.be.eql("((-1)ab+(a+b)^2)");
-    });
-
-    it("factorize a^2+2*a*b+b^2", function() {
-      var term = new TermBracket().fromString("a^2+2*a*b+b^2");
-      term = term.simplify();
-      term = term.factorize();
-      term.sortAndStringify();
-      term.fullStr.should.be.eql("(a+b)^2");
-    });
+    for(var i = 0; i < tests.length; i++) {
+      (function() {
+        var test = tests[i];
+        it("factorize, " + test.input + "  ==>  " + test.output, function() {
+          var term = new TermBracket().fromString(test.input);
+          term = term.simplify();
+          term = term.factorize();
+          term.sortAndStringify();
+          term.fullStr.should.be.eql(test.output);
+        });
+      })();
+    }
   });
 }
--- tests/utils/utils-heap.js
+++ tests/utils/utils-heap.js
@@ -4,84 +4,119 @@ should = require("should");
 
 module.exports = function() {
   describe("heap.js", function() {
-    it("min heap (defaut)", function() {
-      var arr = [];
-      heap.insert(arr, 3);
-      heap.insert(arr, 5);
-      heap.insert(arr, 2);
-      heap.insert(arr, 8);
-      heap.insert(arr, 6);
-
-      heap.deleteEle(arr).should.be.eql(2);
-      heap.deleteEle(arr).should.be.eql(3);
-      heap.deleteEle(arr).should.be.eql(5);
-      heap.deleteEle(arr).should.be.eql(6);
-      heap.deleteEle(arr).should.be.eql(8);
-    });
-
-    it("max heap (with comparator)", function() {
-      var arr = [],
-      comp = function(a, b) {
+    var tests = [{
+      title : "min heap (defaut)",
+      isSimple : 1,
+      insert : [3, 5, 2, 8, 6],
+      remove : [2, 3, 5, 6, 8],
+    }, {
+      title : "max heap (with comparator)",
+      isSimple : 1,
+      insert : [3, 5, 2, 8, 6],
+      remove : [8, 6, 5, 3, 2],
+      comp : function(a, b) {
         return a - b;
-      };
-      heap.insert(arr, 2, comp);
-      heap.insert(arr, 8, comp);
-      heap.insert(arr, 1, comp);
-      heap.insert(arr, 4, comp);
-      heap.insert(arr, 6, comp);
-
-      heap.deleteEle(arr, comp).should.be.eql(8);
-      heap.deleteEle(arr, comp).should.be.eql(6);
-      heap.deleteEle(arr, comp).should.be.eql(4);
-      heap.deleteEle(arr, comp).should.be.eql(2);
-      heap.deleteEle(arr, comp).should.be.eql(1);
-    });
-
-    it("heap with random access change to move up", function() {
-      var arr = [],
-      comp = function(a, b) {
+      },
+    }, {
+      title : "heap with random access change to move up",
+      insert : [3, 5, 2, 8, 6],
+      modify : [{
+        idx : 2,
+        val : 10,
+      }],
+      remove : [10, 8, 6, 5, 3],
+      comp : function(a, b) {
         return a.v - b.v;
-      };
-      heap.insert(arr, {v : 3}, comp);
-      heap.insert(arr, {v : 5}, comp);
-      heap.insert(arr, {v : 2}, comp);
-      heap.insert(arr, {v : 8}, comp);
-      heap.insert(arr, {v : 6}, comp);
-
-      arr[2].v = 10;
-
-      heap.modified(arr, arr[2], comp);
-      heap.deleteEle(arr, comp).should.have.property("v", 10);
-      heap.deleteEle(arr, comp).should.have.property("v", 8);
-      heap.deleteEle(arr, comp).should.have.property("v", 6);
-      heap.deleteEle(arr, comp).should.have.property("v", 5);
-      heap.deleteEle(arr, comp).should.have.property("v", 3);
-    });
-
-    it("heap with random access change to move down", function() {
-      var arr = [],
-      comp = function(a, b) {
+      },
+    }, {
+      title : "heap with random access change to move down",
+      insert : [3, 5, 2, 8, 6],
+      modify : [{
+        idx : 1,
+        val : 1,
+      }],
+      remove : [8, 5, 3, 2, 1],
+      comp : function(a, b) {
         return a.v - b.v;
-      };
-      heap.insert(arr, {v : 3}, comp);
-      heap.insert(arr, {v : 5}, comp);
-      heap.insert(arr, {v : 2}, comp);
-      heap.insert(arr, {v : 8}, comp);
-      heap.insert(arr, {v : 6}, comp);
+      },
+    }, {
+      title : "heap with random access remove #1",
+      insert : [3, 5, 2, 8, 6],
+      removeIdxs : [1],
+      remove : [8, 5, 3, 2],
+      comp : function(a, b) {
+        return a.v - b.v;
+      },
+    }, {
+      title : "heap with random access remove #2",
+      insert : [3, 5, 2, 8, 6],
+      removeIdxs : [4],
+      remove : [8, 6, 3, 2],
+      comp : function(a, b) {
+        return a.v - b.v;
+      },
+    }, {
+      title : "heap with random access remove #3",
+      insert : [3, 5, 2, 8, 6],
+      removeIdxs : [2],
+      remove : [8, 6, 5, 3],
+      comp : function(a, b) {
+        return a.v - b.v;
+      },
+    }, {
+      title : "heap with random access remove #3",
+      insert : [3, 5, 2, 8, 6],
+      removeIdxs : [0],
+      remove : [6, 5, 3, 2],
+      comp : function(a, b) {
+        return a.v - b.v;
+      },
+    }];
 
-      arr[1].v = 1;
+    for(var i = 0; i < tests.length; i++) {
+      (function() {
+        var test = tests[i];
+        it(test.title, function() {
+          var arr = [];
 
-      heap.modified(arr, arr[2], comp);
-      heap.deleteEle(arr, comp).should.have.property("v", 8);
-      heap.deleteEle(arr, comp).should.have.property("v", 5);
-      heap.deleteEle(arr, comp).should.have.property("v", 3);
-      heap.deleteEle(arr, comp).should.have.property("v", 2);
-      heap.deleteEle(arr, comp).should.have.property("v", 1);
-    });
+          for(var j = 0; j < test.insert.length; j++) {
+            if(test.isSimple) {
+              heap.insert(arr, test.insert[j], test.comp);
+            }
+            else {
+              heap.insert(arr, {v : test.insert[j]}, test.comp);
+            }
+          }
+
+          if(test.modify) {
+            for(var k = 0; k < test.modify.length; k++) {
+              arr[test.modify[k].idx].v = test.modify[k].val;
+              heap.modified(arr, arr[test.modify[k].idx], test.comp);
+            }
+          }
+
+          //console.log(arr);
+          if(test.removeIdxs) {
+            for(var j = 0; j < test.removeIdxs.length; j++) {
+              heap.removeEle(arr, arr[test.removeIdxs[j]], test.comp);
+            }
+          }
+
+          for(var j = 0; j < test.remove.length; j++) {
+            if(test.isSimple) {
+              heap.remove(arr, test.comp).should.be.eql(test.remove[j]);
+            }
+            else {
+              heap.remove(arr, test.comp).should.have.property("v", test.remove[j]);
+            }
+          }
+        });
+      })();
+    }
 
     it("delete on empty array", function() {
       var arr = [];
-      should(heap.deleteEle(arr)).be.eql(null);
+      should(heap.remove(arr)).be.eql(null);
     });
   });
 }
